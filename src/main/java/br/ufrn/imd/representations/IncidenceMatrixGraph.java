a46b6b14f354754df1f82d911cd6fcba0101a61b
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-10-28 11:00:54 -0300

    Implement findAdjacentVertices for Incidence Matrix representation to support bfs

--- src/main/java/br/ufrn/imd/representations/IncidenceMatrixGraph.java
+++ src/main/java/br/ufrn/imd/representations/IncidenceMatrixGraph.java
@@ -14,6 +14,30 @@ public class IncidenceMatrixGraph extends Graph {
         this.incidenceMatrix = new ArrayList<>(); // Inicializa a matriz de incidência como lista de listas
     }
 
+    @Override
+    protected List<String> findAdjacentVertices(String vertex) {
+        int vertexIndex = vertices.indexOf(vertex);
+        List<String> adjacentVertices = new ArrayList<>();
+
+        if (vertexIndex == -1) {
+            return adjacentVertices; // Vértice não encontrado
+        }
+
+        // Iterar sobre a matriz de incidência para encontrar arestas que incluem o vértice
+        for (List<Integer> row : incidenceMatrix) {
+            if (row.get(vertexIndex) == 1) {
+                // Achar o outro vértice na aresta
+                for (int i = 0; i < row.size(); i++) {
+                    if (i != vertexIndex && row.get(i) == 1) {
+                        adjacentVertices.add(vertices.get(i));
+                    }
+                }
+            }
+        }
+        return adjacentVertices;
+    }
+
+
     // Método para adicionar uma aresta entre dois vértices em um grafo não direcionado
     @Override
     public void addEdge(String source, String destination) {
