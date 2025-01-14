78b78c66bc8951a83c0e438b5b92cabdae020117
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2025-01-14 17:25:18 -0300

    Add basic time measure methods and solution structure

--- src/main/java/br/ufrn/imd/solutions/SolutionsGraph3.java
+++ src/main/java/br/ufrn/imd/solutions/SolutionsGraph3.java
@@ -3,40 +3,102 @@ package br.ufrn.imd.solutions;
 import br.ufrn.imd.representations.AdjacencyMatrixGraph;
 import br.ufrn.imd.utils.CostGraphFileReader;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.OptionalDouble;
+
 
 public class SolutionsGraph3 {
     private AdjacencyMatrixGraph graph;
 
     private final CostGraphFileReader costGraphFileReader;
+    private long startTime;
+    private long endTime;
+    private final List<Long> times;
+    private static final int EXECUTIONS_AMOUNT = 3000;
 
     public SolutionsGraph3() {
         this.costGraphFileReader = new CostGraphFileReader();
+        this.times = new ArrayList<>();
     }
 
     public void solution1(String fileName) {
-        costGraphFileReader.setFileName(fileName);
-        graph = new AdjacencyMatrixGraph();
-        costGraphFileReader.read(graph);
+        readGraphData(fileName);
+        System.out.println("Executando Algoritmo Guloso...");
+        startTimer();
+        // TODO: Algoritmo Guloso
+        endTimer();
+        printElapsedTime();
 
-
+//        System.out.println("Executando Busca Local...");
+//        startTimer();
+        // TODO: Busca Local
+//        endTimer();
+//        printElapsedTime();
     }
 
     public void solution2(String fileName) {
-        costGraphFileReader.setFileName(fileName);
-        graph = new AdjacencyMatrixGraph();
-        costGraphFileReader.read(graph);
+        readGraphData(fileName);
+        System.out.println("Executando Algoritmo da Inserção mais barata...");
 
+        startTimer();
+        // TODO: Algoritmo da Inserção mais barata
+        endTimer();
+        printElapsedTime();
 
+//        System.out.println("Executando Busca Local...");
+//        startTimer();
+        // TODO (OPT): Busca Local
+//        endTimer();
+//        printElapsedTime();
     }
 
     public void solution3(String fileName) {
+        readGraphData(fileName);
+        System.out.println("Executando Algoritmo GRASP...");
+
+        for (int i = 0; i < EXECUTIONS_AMOUNT; i++) {
+            startTimer();
+            // TODO: Algoritmo GRASP
+            endTimer();
+            times.add(endTime - startTime);
+        }
+        printAverageElapsedTime();
+
+        System.out.println("Executando Busca Local...");
+        startTimer();
+        // TODO: Busca Local
+        endTimer();
+        printElapsedTime();
+    }
+
+    private void readGraphData(String fileName) {
         costGraphFileReader.setFileName(fileName);
         graph = new AdjacencyMatrixGraph();
         costGraphFileReader.read(graph);
-
-
     }
 
+    private void startTimer() {
+        startTime = System.nanoTime();
+    }
+
+    private void endTimer() {
+        endTime = System.nanoTime();
+    }
+
+    private void printElapsedTime() {
+        long elapsedTime = endTime - startTime;
+        System.out.println("Tempo percorrido: " + elapsedTime + " nanosegundos");
+    }
+
+    private void printAverageElapsedTime() {
+        OptionalDouble average = times.stream().mapToLong(Long::longValue).average();
+        if (average.isPresent()) {
+            System.out.println("Tempo médio: " + average.getAsDouble() + " nanosegundos");
+        } else {
+            System.out.println("Tempo médio: Não há valores para calcular a média");
+        }
+    }
 
     public void setGraph(AdjacencyMatrixGraph graph) {
         this.graph = graph;
